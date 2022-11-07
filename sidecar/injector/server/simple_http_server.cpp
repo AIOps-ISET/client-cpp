b45b4e4e20cbf5a2bdf1e1955236269f0b4538d7
shejialuo <shejialuo@gmail.com>
2022-11-07 14:59:10 +0800

    feature(sidecar): add server start code (#14)

---
+++ sidecar/injector/server/simple_http_server.cpp
@@ -0,0 +1,1 @@
+#include "simple_http_server.hpp"
---
+++ sidecar/injector/server/simple_http_server.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file simple_http_server.hpp
+ * @brief a simple http server
+ *
+ */
+
+#ifndef _SIMPLE_HTTP_HPP_
+#define _SIMPLE_HTTP_HPP_
+
+#endif  // _SIMPLE_HTTP_HPP_
